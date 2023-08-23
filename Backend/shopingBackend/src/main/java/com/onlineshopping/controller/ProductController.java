ac7cd7741ead70789482d6919ca7332aec887512
Hemanthkumar432 <hemanth.4324@gmail.com>
2023-08-23 09:58:14 +0530

    CrossOrigin changes for frontend

--- Backend/shopingBackend/src/main/java/com/onlineshopping/controller/ProductController.java
+++ Backend/shopingBackend/src/main/java/com/onlineshopping/controller/ProductController.java
@@ -29,7 +29,7 @@ import com.onlineshopping.utility.StorageService;
 
 @RestController
 @RequestMapping("api/product")
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "https://front-end-shopping.vercel.app/")
 public class ProductController {
 	
 	@Autowired
